9f6952a43eb49bfd82ea662df0957a13401dec76
Sajidur Rahman <shajidur171@gmail.com>
2023-01-28 00:56:36 +0600

    time to finish today's code

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="TemplateMo">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap"
+        rel="stylesheet">
 
-    <title>Stand CSS Blog by TemplateMo</title>
+    <title>File</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -17,19 +17,56 @@
     <!-- Additional CSS Files -->
     <link rel="stylesheet" href="assets/css/fontawesome.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-  </head>
-  <body>
+</head>
+
+<body>
+    <!-- Error Message Start -->
+    <h4 id="error"></h4>
+    <!-- Error Message End -->
+
+
+    <div class="section_wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-mad-12">
+                    <div class="file_uploading_area">
+                        <form action="upload.php" enctype="multipart/form-data" method="post" id="form">
+                            <input type="file" id="theimg">
+                            <input type="submit" id="submit" value="Upload">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
 
-  
-
+    <!-- Custom JS -->
+    <script src="/assets/js/custom.js"></script>
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Additional Scripts -->
-  </body>
+    <script>
+        $(document).ready(function () {
+
+            $("#submit").click(function (e) {
+                e.preventDefault();
+
+                let form_data = new FormData();
+                let img = $("#theimg")[0].files;
+
+                // Checking if image is selected or not
+                if (img.length > 0) {
+
+                }else{
+                    $("#error").text("Please pick an image to display");
+                }
+            });
+        });
+    </script>
+</body>
+
 </html>
